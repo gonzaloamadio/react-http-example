660dbcfe48d63967c6ddf616d03236afe3525e98
gonzaloamadio <gonzaloamadio@gmail.com>
2019-08-13 20:45:57 +1000

    Interceptor, basic example of how to declare it. Global request manipulation.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios'
+
+// This will affect all files in project
+// This one is globally for REQUEST sends.
+// It will take 1 or 2 functions. First one is for request config,
+// second one it is for error handling.
+// NOTE: The error function here, is only for SEND errors, example: no Internet
+axios.interceptors.request.use(request => {
+    console.log(request)
+    
+    // Edit request config 
+
+    // MUST return it, if not request is blocked. It is like a middleware.
+    return request
+}, error => {
+    console.log(error)
+    // Forward to request in the component.
+    return Promise.reject(error)
+})
+
+// This one is globally for RESPONSES.
+axios.interceptors.response.use(request => {
+    console.log(request)
+    // Edit request config 
+    return request
+}, error => {
+    console.log(error)
+    return Promise.reject(error)
+})
+
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
