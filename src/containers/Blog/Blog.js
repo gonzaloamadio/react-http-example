38ef9e8c81929d5ad323d6ccc198fa6ee3a4681f
gonzaloamadio <gonzaloamadio@gmail.com>
2019-09-10 23:22:06 +1000

    Modify layout, and add basic routing, basic NAV menu. SPA app.
     Changes to be committed:
    	modified:   package.json
    	modified:   src/App.js
    	modified:   src/containers/Blog/Blog.css
    	modified:   src/containers/Blog/Blog.js
    	renamed:    src/components/FullPost/FullPost.css -> src/containers/FullPost/FullPost.css
    	renamed:    src/components/FullPost/FullPost.js -> src/containers/FullPost/FullPost.js
    	renamed:    src/components/NewPost/NewPost.css -> src/containers/NewPost/NewPost.css
    	renamed:    src/components/NewPost/NewPost.js -> src/containers/NewPost/NewPost.js
    	new file:   src/containers/Posts/Posts.css
    	new file:   src/containers/Posts/Posts.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,70 +1,31 @@
 import React, { Component } from 'react';
-// import axios from 'axios'
-import axios from '../../axios'
+// import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from '../Posts/Posts';
+import NewPost from '../NewPost/NewPost';
 
 class Blog extends Component {
-    state = {
-        posts : [],
-        selectedPostId : null,
-        apiError : false
-    }
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                // Fake Pagination, or can do it like this, if we knoe there wont be too much
-                // So we do not keep sending requests
-                const posts = response.data.slice(0,6)
-                // We can transform our data, it is still JS!
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author : 'Gonzalo'
-                    }
-                })
-                this.setState({posts: updatedPosts})
-            })
-            .catch(error => {
-                this.setState({apiError:true})
-            })
-    }
-
-    postSelectedHandler = id => {
-        this.setState({selectedPostId : id})
-
-    }
-
     render () {
-        let posts = <p style={{textAlign:'center'}}>Something went wrong</p>
-        if (!this.state.apiError) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title}
-                    author={post.author}
-                    // Arrow function to pass id selected
-                    clicked={() => this.postSelectedHandler(post.id)}
-                    />
-            })    
-        }
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    {/* Added selected post to state, so we can pass it down to FullPost Component */}
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
