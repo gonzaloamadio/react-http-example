658981daaa79cfa463302c7448221cf109de7273
gonzaloamadio <gonzaloamadio@gmail.com>
2019-10-01 09:24:15 +1000

    Routing: Catch all routes, 404. Redirect and Route methods.
     Changes to be committed:
    	modified:   src/containers/Blog/Blog.js

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
+// eslint-disable-next-line
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
@@ -7,32 +8,54 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><NavLink to="/posts" exact>Home</NavLink></li>
-                            <li><NavLink to={{
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</NavLink></li>
-                        </ul>
-                    </nav>
-                </header>
-                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+  state = {
+    auth: false
+  };
+  render() {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink to="/posts" exact>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true'
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                {/* With Switch, stop analyzing routes when first match is encountered */}
-                <Switch> 
-                    <Route path="/new-post" component={NewPost} />
-                    <Route path="/posts"  component={Posts} />
-                    <Redirect from="/" to="/posts"/>
-                </Switch>
-            </div>
-        );
-    }
+        {/* With Switch, stop analyzing routes when first match is encountered */}
+        <Switch>
+          {/* This is how we control a lot of stuff, not rendering components if some condition happens */}
+          {/* This will give null if not auth, and will end up in the Redirect or Route without path, 
+                that catch everything that was not cought before. So we will end in /posts */}
+          {this.state.auth ? (
+            <Route path="/new-post" component={NewPost} />
+          ) : null}
+          <Route path="/posts" component={Posts} />
+          {/* This is one way, with the Redirect at the end to catch a 404, 
+          all unknown pages will be redirected with this. */}
+          {/* <Redirect from="/" to="/posts" /> */}
+          {/* Another way of doing it, is having a route withouth path */}
+          <Route render={() => <h1>Not Found</h1>} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default Blog;
